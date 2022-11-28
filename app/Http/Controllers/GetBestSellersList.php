833c95a74c72685bbbed2e505daa450a0372bee3
speccode <github@speccode.com>
2022-11-28 22:03:10 +0100

    cleanup unused routes, adds single action controller for getting bestsellers

---
+++ app/Http/Controllers/GetBestSellersList.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class GetBestSellersList extends Controller
+{
+    public function __invoke()
+    {
+        // TODO: Implement __invoke() method.
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\GetBestSellersList;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,7 +13,4 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/api/1/nyt/best-sellers', GetBestSellersList::class);
