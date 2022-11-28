3b450b14282bd925a8cd8bd70531cc1f571a6ded
speccode <github@speccode.com>
2022-11-28 22:15:48 +0100

    adds GetBestSellersRequest

---
+++ src/BestSellers/Infrastructure/Http/Requests/GetBestSellersRequest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Speccode\BestSellers\Infrastructure\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class GetBestSellersRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules(): array
+    {
+        return [
+
+        ];
+    }
+}
---
+++ src/BestSellers/ModuleServiceProvider.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Speccode\BestSellers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ModuleServiceProvider extends ServiceProvider
+{
+
+}
